31134e3e1383378d1fbaa42e7ddb1452098571c8
zholdsworth <zholdsworth@unomaha.edu>
2023-04-21 20:13:59 -0500

    Added first file

---
+++ account.py
@@ -0,0 +1,15 @@
+class Account:
+    def __init__(self, name):
+        pass
+    
+    def deposit(self, amount):
+        pass
+    
+    def withdraw(self, amount):
+        pass
+    
+    def get_balance(self):
+        pass
+    
+    def get_name(self):
+        pass
