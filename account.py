bb6e67d1fe23aa4d6476999ccb91e07c8885b9da
zholdsworth <zholdsworth@unomaha.edu>
2023-04-23 18:08:57 -0500

    Changed variables to private. self.account_name to self.__account_name and self.account_balance to self.__account_balance.

--- account.py
+++ account.py
@@ -4,8 +4,8 @@ class Account:
         Function to set up object
         :param name: Account name
         """
-        self.account_name = name
-        self.account_balance = 0.0
+        self.__account_name = name
+        self.__account_balance = 0.0
     
     def deposit(self, amount: float) -> bool:
         """
@@ -16,19 +16,19 @@ class Account:
         if amount <= 0:
             return False
         else:
-            self.account_balance += amount
+            self.__account_balance += amount
             return True
 
     def withdraw(self, amount: float) -> bool:
         """
         Withdraws the specified amount from the account.
-        :param amount: float
+        :param amount: Account balance
         :return: bool, True if withdrawal successful, False if not.
         """
-        if amount <= 0 or amount > self.account_balance:
+        if amount <= 0 or amount > self.__account_balance:
             return False
         else:
-            self.account_balance -= amount
+            self.__account_balance -= amount
             return True
     
     def get_balance(self) -> float:
@@ -36,11 +36,11 @@ class Account:
         Returns the current balance in the account.
         :return: self.balance
         """
-        return self.account_balance
+        return self.__account_balance
 
     def get_name(self) -> str:
         """
         Returns the name of the account holder.
         :return: self.name
         """
-        return self.account_name
+        return self.__account_name
